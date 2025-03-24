fe15600131f8642e57ebb9e6ce863971eb2c9de7
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-03-23 18:22:24 -0700

    implement get_or_fetch_movie function to retrieve movie details from repository or external API

--- app/services/movie_service.py
+++ app/services/movie_service.py
@@ -1,6 +1,22 @@
+from app.db.models import Movie
 from app.external.api_client import fetch_movie_details_safe
 from app.repositories import movie_repository
 
 def get_or_fetch_movie(movie_id: int):
-    # TODO
-    ...
+    movie = movie_repository.get_movie_by_id(movie_id)
+    if movie:
+        return movie
+
+    api_data = fetch_movie_details_safe(movie_id)
+    movie = Movie(
+        id=api_data["id"],
+        title=api_data["title"],
+        description=api_data.get("overview"),
+        release_date=api_data.get("release_date"),
+        popularity=api_data.get("popularity"),
+        vote_average=api_data.get("vote_average"),
+        vote_count=api_data.get("vote_count"),
+        genre_ids=str(api_data.get("genre_ids"))  # store list as string
+    )
+    # return movie_repository.insert_movie(movie)
+    print(f"Movie not found. Calling TMDB's API instead.\n{api_data.json()}")
