3f2350217eab6ddc6938597686402000ba8561c4
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-03-23 17:02:26 -0700

    refactor database module to use SQLModel for database interactions and create session management

--- app/db/database.py
+++ app/db/database.py
@@ -1,24 +1,10 @@
-import sqlite3
-import csv
-import pandas as pd
+from sqlmodel import SQLModel, create_engine, Session
 
+DATABASE_URL = "sqlite:///./app/db/database.db"
+engine = create_engine(DATABASE_URL, echo=True)
 
-def fetch_one_movie():
-    with sqlite3.connect("database.db") as conn:
-        
-        cur = conn.cursor()
-        
-        # Drop the table if it exists
-        # cur.execute("DROP TABLE IF EXISTS movie")
-        # cur.execute("CREATE TABLE movie(id, genre_ids, overview, popularity, release_date, title, vote_average, vote_count)")
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
-        # Execute a query to fetch one row
-        cur.execute("SELECT * FROM movie LIMIT 1")
-        
-        # Fetch one row
-        row = cur.fetchone()
-        
-        if row:
-            print("Fetched movie:", row)
-        else:
-            print("No movie found")
+def get_session():
+    return Session(engine)
