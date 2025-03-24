89dd151ac42ad612e4decae1e44bc3865d1734d0
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-03-23 18:22:13 -0700

    implement get_movie_by_id and insert_movie functions in movie_repository.py

--- app/repositories/movie_repository.py
+++ app/repositories/movie_repository.py
@@ -3,5 +3,12 @@ from app.db.models import Movie
 from sqlmodel import select
 
 def get_movie_by_id(movie_id: int):
-    # TODO
-    ...
+    with get_session() as session:
+        return session.exec(select(Movie).where(Movie.id == movie_id)).first()
+
+def insert_movie(movie: Movie):
+    with get_session() as session:
+        session.add(movie)
+        session.commit()
+        session.refresh(movie)
+        return movie
