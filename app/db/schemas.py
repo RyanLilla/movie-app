5c6cae600b1e1b6d70e8953ce4676a769b97dc19
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-03-30 11:49:51 -0700

    add MovieRead schema fields and define MovieCreate and MovieUpdate schemas

--- app/db/schemas.py
+++ app/db/schemas.py
@@ -2,5 +2,15 @@ from sqlmodel import SQLModel
 from typing import Optional
 
 class MovieRead(SQLModel):
+    id: int
+    title: str
+    overview: Optional[str] = None
+    release_date: Optional[str] = None
+
+class MovieCreate(SQLModel):
+    # TODO
+    ...
+
+class MovieUpdate(SQLModel):
     # TODO
     ...
