5b41f1328c0e40aa665ec2f19b66cf75ceef99d4
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-03-30 10:34:35 -0700

    add main function to retrieve and display movie details using get_or_fetch_movie

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+from pprint import pprint
 from fastapi import FastAPI
 
-app = FastAPI()
+from services.movie_service import get_or_fetch_movie
+
+# app = FastAPI()
 
 
 def main():
-    ...
+    # movie_id = input("Enter a movie ID to begin: ")
+    response = get_or_fetch_movie(movie_id=1013850)
+    pprint(response)
     
 if __name__ == "__main__":
     main()
