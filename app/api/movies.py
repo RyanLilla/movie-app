728595b90d566b31dc41a8756ec07c3d6784cb00
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-03-30 17:01:59 -0700

    add endpoint to retrieve movie by ID and set up FastAPI router

--- app/api/movies.py
+++ app/api/movies.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
+from app.db.schemas import MovieResponse
+from app.services.movie_service import get_or_fetch_movie
 
 
 router = APIRouter()
 
-@router.get("/movie/", tags=["movie"])
+@router.get("/movie")
 async def get_movie_by_id():
-    ...
+    return {"message": "Hello, Movie App : Movie Controller"}
+
+@router.get("/movie/{movie_id}", response_model=MovieResponse)
+async def get_movie_by_id(movie_id: int):
+    movie = get_or_fetch_movie(movie_id=movie_id)
+    if not movie:
+        raise HTTPException(status_code=404, detail="No movie found for the specified ID.")
+    
+    return movie
--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
+from contextlib import asynccontextmanager
 from pprint import pprint
 from fastapi import FastAPI
 
-from services.movie_service import get_or_fetch_movie
+from .api import movies
+from .db.database import create_db_and_tables
+# from .services.movie_service import get_or_fetch_movie
 
-# app = FastAPI()
 
-
-def main():
-    # movie_id = input("Enter a movie ID to begin: ")
-    response = get_or_fetch_movie(movie_id=1013850)
-    pprint(response)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Perform startup tasks like creating the database tables
+    create_db_and_tables()
     
-if __name__ == "__main__":
-    main()
+    # Yield control back to FastAPI to start processing requests
+    yield
+    
+app = FastAPI(lifespan=lifespan)
+app.include_router(movies.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, Movie App"}
+
+# def main():
+#     # movie_id = input("Enter a movie ID to begin: ")
+#     response = get_or_fetch_movie(movie_id=1013850)
+#     pprint(response)
+#     
+# if __name__ == "__main__":
+#     main()
