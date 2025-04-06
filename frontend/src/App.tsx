2f3918bec9ce1d6bc1c85834e965910c399e37c8
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-04-06 16:10:33 -0700

    Add initial styling changes for how the navbar is displayed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
-import SearchMovie from './components/SearchMovie';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SearchMovie from "./components/SearchMovie";
+import Home from "./components/Home";
+import About from "./components/About";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Movie Lookup</h1>
-      <SearchMovie />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className="page-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchMovie />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
