474e68e52600a662df82e58358fc480bcd0e32b0
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-04-13 11:18:20 -0700

    Add Genre and MovieGenreLink SQLModel classes

--- app/db/models.py
+++ app/db/models.py
@@ -13,6 +13,27 @@ class Movie(MovieBase, table=True):
     popularity: Optional[int] = None
     poster_url: Optional[str] = None
     backdrop_url: Optional[str] = None
+    # adult: Optional[bool] = None
+    # budget: Optional[int] = None
+    # homepage: Optional[str] = None
+    # imdb_id: Optional[str] = None
+    # original_language: Optional[str] = None
+    # original_title: Optional[str] = None
+    # revenue: Optional[int] = None
+    # runtime: Optional[int] = None
+    # status: Optional[str] = None
+    # tagline: Optional[str] = None
+    # video: Optional[bool] = None
+    
+class Genre(SQLModel, table=True):
+    __tablename__ = "genre"
+    id: int = Field(primary_key=True)
+    name: str
+
+class MovieGenreLink(SQLModel, table=True):
+    __tablename__ = "movie_genre_link"
+    movie_id: int = Field(foreign_key="movie.id", primary_key=True)
+    genre_id: int = Field(foreign_key="genre.id", primary_key=True)
 
 class ProductionCompany(SQLModel, table=True):
     __tablename__ = "production_company"
